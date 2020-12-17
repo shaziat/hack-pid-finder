466e6f14ebc1e2e826a33ce91aec0c756d5e537f
TruptiGarotkar03 <trupti.garotkar02.ext@bbc.co.uk>
2020-12-17 12:24:11 +0000

    rebasing

--- step-definitions/pid_finder.js
+++ step-definitions/pid_finder.js
@@ -1,12 +1,12 @@
 const request = require('supertest');
 
-const url = 'https://rms.api.bbc.co.uk/v2/experience/inline/play';
+const url = 'https://rms.api.bbc.co.uk/v2/experience/inline/play/';
 module.exports = function () {
 
     this.Given(/^I query RMS API$/, () => helpers.loadPage(page.pidFinder.url, 1000));
 
     this.When(/^I extract PID$/, () => {
-        //  let elemArray = driver.findElements(page.pidFinder.elements.musicPid);
+         let elemArray = driver.findElements(page.pidFinder.elements.musicPid);
         const elemArray = driver.findElements(page.pidFinder.elements.musicPid);
         return elemArray.then(function(elems) {
             return Promise.all(
@@ -24,36 +24,40 @@ module.exports = function () {
     });
 
     this.Then(/^I can see its available for at least more than twenty days$/, async function () {
-        await request(url)
-            .get('/m000plcn')
-            .expect(200)
-            .expect('Content-Type', /json/)
-            .then((response) => {
+        const arr = ['b0080x5m', 'm000qc5m', 'm000qbhr'];
+        arr.forEach(async(pid) => {
+            await request(url)
+                .get(pid)
+                .expect('Content-Type', /json/)
+                .then((response) => {
+                    console.log(response.status);
+                    if (response.status === 200) {
+                        response.body.data.forEach((data) => {
+                            data.data.forEach((innerData) => {
 
-                response.body.data.forEach((data) => {
-                    data.data.forEach((innerData) => {
+                                if (innerData) {
+                                    const availablity = innerData.availability;
+                                    if (availablity) {
+                                        const toDateString = availablity.to;
 
-                        if (innerData) {
-                            const availablity = innerData.availability;
-                            if (availablity) {
-                                const toDateString = availablity.to;
+                                        const currentDate = new Date();
+                                        const toDate = new Date(toDateString);
+                                        var time = Math.floor((toDate - currentDate) / (24 * 3600 * 1000));
 
-                                const currentDate = new Date();
-                                const toDate = new Date(toDateString);
-                                var time = Math.floor((toDate - currentDate) / (24 * 3600 * 1000));
-
-                                if (time > 20) {
-                                    console.log('this pid availability for ' + time + 'days');
+                                        if (time > 20) {
+                                            console.log('this pid availability for ' + time + 'days');
+                                        }
+                                        else {
+                                            console.log('this pid availability for ' + time + 'days');
+                                        }
+                                    }
                                 }
-                                else {
-                                    console.log('this pid availability for ' + time + 'days');
-                                }
-                            }
-                        }
 
-                    });
+                            });
+                        });
+                    }
+
                 });
-
-            });
+        });
     });
 };
